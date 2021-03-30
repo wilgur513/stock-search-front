0feb0372068858ebbf575b99fa358974cca32b54
wilgur513 <wilgur513@naver.com>
2021-03-30 21:54:59 +0900

    StockTableView로 변경

---
+++ src/components/StocksTableView.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Attributes = ({filterList}) => (
+  filterList.map(list => <th key={list.id} id={list.id} data-expression={list.expression}> {list.name} </th>)
+)
+
+const TableHead = ({filterList}) => (
+  <thead>
+    <tr>
+      <Attributes filterList={filterList} />
+    </tr>
+  </thead>
+)
+
+const TableBody = () => (
+  <tbody>
+    <tr>
+      <td />
+    </tr>
+  </tbody>
+)
+
+export const StocksTableView = ({filterList})=>{
+  return(
+    <table>
+      <TableHead filterList={filterList} />
+      <TableBody />
+    </table>
+  )
+}
